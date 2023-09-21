43fb9b5fe36bcda0dc3c7c876ad71cf07d930d57
Ryan Sawadichai <raycosm@gmail.com>
2023-09-21 12:17:12 -0600

    added dotenv

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,1 +1,23 @@
-console.log("hello world");
+import express from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.SERVER_PORT || 3000;
+console.log(PORT);
+const app = express();
+app.use(express.json());
+// app.use(express.urlencoded());
+
+app.get('/',(req, res) => {
+  res.send('Hello World');
+});
+
+/**
+ * 404 handler
+ */
+app.use('*', (req,res) => {
+  res.status(404).send('Not Found');
+});
+
+app.listen(PORT, ()=>{ console.log(`Listening on port ${PORT}...`); });
+
