b0717e35421a9731fd081591fb557bb662ba5ef6
Ryan Sawadichai <raycosm@gmail.com>
2023-09-21 13:27:46 -0600

    set up express static file serving

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,16 +1,26 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import * as path from 'path';
 
+// ES6 compatibility for __dirname
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Configure environment variables
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 3000;
-console.log(PORT);
+
 const app = express();
+
+// Middleware
 app.use(express.json());
 // app.use(express.urlencoded());
 
-app.get('/',(req, res) => {
-  res.send('Hello World');
-});
+// Serve static client files
+app.use(express.static(path.resolve(__dirname, '../../dist/client/')))
+
+// Routes
 
 /**
  * 404 handler
@@ -20,4 +30,3 @@ app.use('*', (req,res) => {
 });
 
 app.listen(PORT, ()=>{ console.log(`Listening on port ${PORT}...`); });
-
