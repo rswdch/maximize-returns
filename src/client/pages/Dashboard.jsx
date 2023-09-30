3dcab84beec3bc12b873464c72fb5bc3ff1647f2
Ryan Sawadichai <raycosm@gmail.com>
2023-09-29 19:54:14 -0600

    Fetch purchases

--- src/client/pages/Dashboard.jsx
+++ src/client/pages/Dashboard.jsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Dashboard = ({user}) => {
+  const [purchases, setPurchases] = useState({});
+  const [isFetched, setFetched] = useState(false);
+
+  useEffect(() => {
+    console.log('The token in fetch is')
+    console.log(localStorage.getItem('token'));
+      fetch(`/api/purchases`, {
+        headers: {
+          'Authorization': localStorage.getItem('token'),
+        }
+      }).then((res) => {
+        return res.json();
+      }).then(data => {
+        setPurchases(data);
+        setFetched(true);
+        console.log(Object.keys(data[0]));
+      })
+  }, [])
+
   return (
     <div>
       <h2>Welcome, {user}</h2>
+      <h3>Here are your products</h3>
+      { isFetched ? purchases.map(el => <tr>
+        <th>{el.name}</th>
+      </tr>) : <p>Loading...</p>}
     </div>
   )
 }
