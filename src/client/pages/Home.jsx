bc4d3e8997bf9b2a4af4aab5620dd246347ad8d4
rswdch <ryan.sawadichai@outlook.com>
2023-12-31 20:24:08 -0700

    tailwind styling hero/main page

--- src/client/pages/Home.jsx
+++ src/client/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { LoginForm } from "./login/LoginForm.jsx";
 
 const Home = () => {
   return (
@@ -14,30 +15,9 @@ const Home = () => {
           <p className="mb-5">
             Blazingly Fast! Built with TypeScript, Express, React, and PostgreSQL
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary">Demo</button>
         </div>
-        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input type="email" placeholder="email" className="input input-bordered" required />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input type="password" placeholder="password" className="input input-bordered" required />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
-            </div>
-          </form>
-        </div>
+        <LoginForm />
       </div>
     </div>
   );
